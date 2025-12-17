f8ef5aafee476ceaf80f3d8f5d63e02e7f9ee017
Aryanji Patel <ap0905749@gmail.com>
2025-12-17 17:33:01 +0530

    Initial second

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar';
+import Banner from './Components/Banner';
 
 const App = () => {
   return (
     <div className='text-bold text-3xl'>
       <Navbar />
+      <Banner />
     </div>
   )
 }
---
+++ Frontend/src/Components/Banner.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import banner from "../../public/Banner.jpg"
+const Banner = () => {
+  return (
+    <>
+      <div className="max-w-screen-2x1 container mx-auto md:px-20 px-4 flex flex-col md:flex-row ">
+        <div className="w-full md:w-1/2 mt-12 md:mt-32">
+        <div className="space-y-12">
+          <h1 className="text-4x1 font-bold">
+            Hello, welcomes here to learn something{" "}
+            <span className="text-pink-500">new everyday !!!</span>
+          </h1>
+      <p className="text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+      Laudantium dolore deleniti officiis inventore facere temporibus,
+       ab neque aliquid mollitia !
+       </p>
+
+       <label className="input validator">
+  <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g
+      strokeLinejoin="round"
+      strokeLinecap="round"
+      strokeWidth="2.5"
+      fill="none"
+      stroke="currentColor"
+    >
+      <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+      <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+    </g>
+  </svg>
+  <input type="email" placeholder="email" required />
+</label>
+  </div>
+       <button className="btn mt-6 btn-secondary ">Secondary</button>
+        </div>
+        <div className="w-full md:w-1/2">
+          <img src={banner}  alt=""/>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Banner;
