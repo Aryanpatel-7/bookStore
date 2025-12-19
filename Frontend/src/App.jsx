12fa7c231d22b0865c3374250bb284bf523ebca4
Aryanji Patel <ap0905749@gmail.com>
2025-12-19 18:05:08 +0530

    create footer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar';
 import Banner from './Components/Banner';
+import Footer from './Components/Footer';
 
 const App = () => {
   return (
     <div className='text-bold text-3xl'>
       <Navbar />
       <Banner />
+      <Footer />
     </div>
   )
 }
--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const Navbar = () => {
+const Navbar = () => { 
+  const [sticky,setsticky]=useState(false);
+  useEffect(()=> {
+  const handleScroll=()=>{
+     if (window.scrollY > 0) {
+        setsticky(true)
+     } else {
+      setsticky(false)
+     }
+  }
+  window.addEventListener('scroll',handleScroll)
+  return ()=>{
+    removeEventListener('scroll',handleScroll)
+  }
+ },[]);
+
   const navItems =(
     <>
        <li><a>Home</a></li>
@@ -11,8 +26,13 @@ const Navbar = () => {
   )
   return (
    <>
-     <div className='max-w-screen-2x1 container mx-auto md:px-20 px-4'>
-       <div className="navbar bg-base-100 shadow-sm">
+     <div className={` max-w-screen-2x1 container mx-auto md:px-20 px-4 fixed top-0 left-0 right-0 ${
+      sticky
+      ?"sticky-navbar shadow-md bg-base-200 duration-300 transition-all ease-in-out"
+      : ""
+     }`}>
+
+       <div className="navbar">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
