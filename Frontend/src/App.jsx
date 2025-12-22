846f6ee25e00cb81aea425ba0a566fbb2d0e6f4c
Aryanji Patel <ap0905749@gmail.com>
2025-12-22 18:25:23 +0530

    create Cards

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './Components/Navbar';
 import Banner from './Components/Banner';
 import Footer from './Components/Footer';
+import Freebook from './Components/Freebook';
 
 const App = () => {
   return (
     <div className='text-bold text-3xl'>
       <Navbar />
       <Banner />
+      <Freebook />
       <Footer />
     </div>
   )
---
+++ Frontend/src/Components/Cards.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Cards = ({item}) => {
+    console.log(item);        // check full object
+  console.log(item?.price); 
+  return (
+  <>
+    <div>
+      <div className="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src={item.image} alt="Book" />
+      
+
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">
+      {item.name}
+      <div className="badge badge-secondary">{item.category}</div>
+    </h2>
+    <p>{item.title}</p>
+    <div className="card-actions justify-between">
+      <div className="badge badge-outline">
+     ${item.price}</div>
+      <div className="badge badge-outline hover:bg-pink-500 hover:text-white duration-200">Buy Now</div>
+    </div>
+  </div>
+</div>
+    </div>
+  </>
+  )
+}
+
+export default Cards;
